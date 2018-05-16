932374b98fec2fa11e65ba00a1f75aaaa7fe2ebf
David Van Der Beek <earlynovrock@gmail.com>
2018-05-16 07:43:05 -0500

    better wording for no start/stop times

--- app/helpers/plumber/campaigns_helper.rb
+++ app/helpers/plumber/campaigns_helper.rb
@@ -1,7 +1,11 @@
 module Plumber
   module CampaignsHelper
     def twelve_hour(hour)
-      if hour <= 12
+      if hour == 0
+        "Beginning of day"
+      elsif hour == 24
+        "End of day"
+      elsif hour <= 12
         "#{hour}am"
       else
         "#{hour - 12}pm"
